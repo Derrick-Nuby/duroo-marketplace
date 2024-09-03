d32293089ae30a851514e3fe8d06ef6b70e617c5
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-09-03 16:44:09 +0200

    ui done

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Input } from '../ui/input';
+
+function NavSearch() {
+    return (
+        <Input type='search' placeholder='search products' className='max-w-xs dark:bg-muted' />
+    );
+}
+
+export default NavSearch;
