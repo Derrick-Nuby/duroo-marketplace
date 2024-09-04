72ffbffd7d8b46b89e6f8546ac147f123ad8326c
Derrick-Nuby <iradukundaderrick7@gmail.com>
2024-09-04 15:23:43 +0200

    project init hard coded values

---
+++ components/home/Hero.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import HeroCarousel from './HeroCarousel';
+import { Button } from '../ui/button';
+import Link from 'next/link';
+
+function Hero() {
+    return (
+        <section className='grid grid-cols-1 lg:grid-cols-2 gap-24 items-center'>
+            <div>
+                <h1 className="max-w-2xl font-bold text-4xl tracking-tight sm:text-6xl">
+                    We are changing the way people shop
+                </h1>
+                <p className='mt-8 max-w-xl text-lg text-muted-foreground'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda accusamus et eveniet autem reprehenderit! Earum minus atque laborum perspiciatis voluptas. Saepe facilis at magni consectetur beatae similique earum debitis temporibus!
+                </p>
+                <Button asChild size={'lg'} className='mt-10'>
+                    <Link href={'/products'}> Our Products</Link>
+                </Button>
+            </div>
+
+            <HeroCarousel />
+        </section>
+    );
+}
+
+export default Hero;
--- components/navbar/NavBar.tsx
+++ components/navbar/NavBar.tsx
@@ -5,13 +5,16 @@ import NavSearch from './NavSearch';
 import CartButton from './CartButton';
 import DarkMode from './DarkMode';
 import LinksDropDown from './LinksDropDown';
+import { Suspense } from 'react';
 
 function NavBar() {
     return (
         <nav className='border-b'>
             <Container className='flex flex-col sm:flex-row sm:justify-between sm:items-center gap-4 flex-wrap py-8'>
                 <Logo />
-                <NavSearch />
+                <Suspense>
+                    <NavSearch />
+                </Suspense>
                 <div className='flex gap-4 items-center'>
                     <CartButton />
                     <DarkMode />
